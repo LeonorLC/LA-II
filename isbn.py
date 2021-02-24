cf41d224f032bf287b54a1d16327fff68750aabd
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-02-24 16:33:26 +0000

    Update isbn.py

--- isbn.py
+++ isbn.py
@@ -6,3 +6,41 @@ cada um multiplicado por um peso que é alternadamente 1 ou 3, seja um múltiplo
 A função recebe um dicionário que associa livros a ISBNs,
 e deverá devolver a lista ordenada de todos os livros com ISBNs inválidos.
 '''
+
+def isbn(livros):
+    numeros=list(livros.values())
+    l=[]
+    
+    for num  in numeros :
+        n = ''
+        lista_cada=[]
+        for digito in num:
+            lista_cada.append(digito)
+        l.append(lista_cada)
+    
+    l_final=[]
+    sumP=sumI=0
+    for lista_cada in l:
+        i=0
+        sumP=sumI=0
+        for digito in lista_cada:
+    
+            if i == 0 or i%2 == 0:
+                sumP += int(digito)*1
+                sumI += int(digito)*3
+            else:
+                sumP += int(digito)*3
+                sumI += int(digito)*1
+            i = i+ 1
+        
+        if sumP%10 != 0 and sumI%10 != 0: 
+            l_final.append(''.join(lista_cada))
+
+    res = []
+    itemsList = livros.items()
+    for num in l_final:
+        for item in itemsList:
+            if item[1] == num:
+                res.append(item[0])
+
+    return sorted(res)
