e765aa873bff5b0588704ed25f04eee1cfef5f06
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-02-24 21:55:32 +0000

    Update repete.py

--- repete.py
+++ repete.py
@@ -2,3 +2,16 @@
 Implemente uma função que determine qual a menor sequência de caracters que
 contém n repetições de uma determinada palavra
 '''
+def repete(palavra,n):
+    if n == 0:
+        return ''
+    p=palavra
+    if palavra[0] == palavra[-1]:
+        while n > 1:
+            p += palavra[1:]
+            n -= 1
+    else:
+        while n > 1:
+            p += palavra
+            n -= 1
+    return p
