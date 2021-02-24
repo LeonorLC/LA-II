141d38d7d1195680ec933596969b270c6286ad5a
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-02-24 10:52:29 +0000

    Create futebol.py
    Implemente uma função que calcula a tabela classificativa de um campeonato de
    futebol. A função recebe uma lista de resultados de jogos (tuplo com os nomes das
    equipas e os respectivos golos) e deve devolver a tabela classificativa (lista com 
    as equipas e respectivos pontos), ordenada decrescentemente pelos pontos. Em
    caso de empate neste critério, deve ser usada a diferença entre o número total
    de golos marcados e sofridos para desempatar, e, se persistir o empate, o nome
    da equipa.

---
+++ futebol.py
@@ -0,0 +1,11 @@
+'''
+
+Implemente uma função que calcula a tabela classificativa de um campeonato de
+futebol. A função recebe uma lista de resultados de jogos (tuplo com os nomes das
+equipas e os respectivos golos) e deve devolver a tabela classificativa (lista com 
+as equipas e respectivos pontos), ordenada decrescentemente pelos pontos. Em
+caso de empate neste critério, deve ser usada a diferença entre o número total
+de golos marcados e sofridos para desempatar, e, se persistir o empate, o nome
+da equipa.
+
+'''
