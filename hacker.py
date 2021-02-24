71e1ca2ae66bd650737438a6a2ad93ec70be5a6d
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-02-24 10:51:45 +0000

    Create hacker.py
    Um hacker teve acesso a um log de transações com cartões de
    crédito. O log é uma lista de tuplos, cada um com os dados de uma transação,
    nomedamente o cartão que foi usado, podendo alguns dos números estar
    ocultados com um *, e o email do dono do cartão.
    
    Pretende-se que implemente uma função que ajude o hacker a 
    reconstruir os cartões de crédito, combinando os números que estão
    visíveis em diferentes transações. Caso haja uma contradição nos números 
    visíveis deve ser dada prioridade à transção mais recente, i.é, a que
    aparece mais tarde no log.
    
    A função deve devolver uma lista de tuplos, cada um com um cartão e um email,
    dando prioridade aos cartões com mais digitos descobertos e, em caso de igualdade
    neste critério, aos emails menores (em ordem lexicográfica).

---
+++ hacker.py
@@ -0,0 +1,16 @@
+"""
+Um hacker teve acesso a um log de transações com cartões de
+crédito. O log é uma lista de tuplos, cada um com os dados de uma transação,
+nomedamente o cartão que foi usado, podendo alguns dos números estar
+ocultados com um *, e o email do dono do cartão.
+
+Pretende-se que implemente uma função que ajude o hacker a 
+reconstruir os cartões de crédito, combinando os números que estão
+visíveis em diferentes transações. Caso haja uma contradição nos números 
+visíveis deve ser dada prioridade à transção mais recente, i.é, a que
+aparece mais tarde no log.
+
+A função deve devolver uma lista de tuplos, cada um com um cartão e um email,
+dando prioridade aos cartões com mais digitos descobertos e, em caso de igualdade
+neste critério, aos emails menores (em ordem lexicográfica).
+"""
