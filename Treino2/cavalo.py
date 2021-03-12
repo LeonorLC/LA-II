a09dd3447988af85c53c9041881f8fc7d671f3c7
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-03-12 22:48:39 +0000

    Create cavalo.py

---
+++ Treino2/cavalo.py
@@ -0,0 +1,10 @@
+'''
+
+O objectivo deste problema é determinar quantos movimentos são necessários para 
+movimentar um cavalo num tabuleiro de xadrez entre duas posições.
+A função recebe dois pares de coordenadas, que identificam a origem e destino pretendido,
+devendo devolver o número mínimo de saltos necessários para atingir o destino a partir da origem.
+Assuma que o tabuleiro tem tamanho ilimitado.
+
+'''
+
