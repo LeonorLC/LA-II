6c6edb2c6870e959263ead1434522fc264c9fe00
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-03-12 22:49:44 +0000

    Create continente.py

---
+++ Treino2/continente.py
@@ -0,0 +1,10 @@
+'''
+
+O objectivo deste problema é determinar o tamanho do maior continente de um planeta.
+Considera-se que pertencem ao mesmo continente todos os países com ligação entre si por terra. 
+Irá receber uma descrição de um planeta, que consiste numa lista de fronteiras, onde cada fronteira
+é uma lista de países que são vizinhos entre si. 
+A função deverá devolver o tamanho do maior continente.
+
+'''
+
