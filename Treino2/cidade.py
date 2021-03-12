67cc06b789382bd4b62220dc74f199af33925c9f
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-03-12 22:49:06 +0000

    Create cidade.py

---
+++ Treino2/cidade.py
@@ -0,0 +1,13 @@
+'''
+
+Podemos usar um (multi) grafo para representar um mapa de uma cidade: 
+cada nó representa um cruzamento e cada aresta uma rua.
+Pretende-se que implemente uma função que calcula o tamanho de uma cidade, 
+sendo esse tamanho a distância entre os seus cruzamentos mais afastados.
+A entrada consistirá numa lista de nomes de ruas (podendo assumir que os 
+nomes de ruas são únicos). Os identificadores dos cruzamentos correspondem a 
+letras do alfabeto, e cada rua começa (e acaba) no cruzamento 
+identificado pelo primeiro (e último) caracter do respectivo nome.
+
+'''
+
