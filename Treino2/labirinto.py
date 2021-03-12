c1e350df9ec0e78537d1f6ec981b6e287e353845
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-03-12 22:50:31 +0000

    Create labirinto.py

---
+++ Treino2/labirinto.py
@@ -0,0 +1,11 @@
+'''
+
+Implemente uma função que calcula um dos caminhos mais curtos para atravessar
+um labirinto. O mapa do labirinto é quadrado e representado por uma lista 
+de strings, onde um ' ' representa um espaço vazio e um '#' um obstáculo.
+O ponto de entrada é o canto superior esquerdo e o ponto de saída o canto
+inferior direito. A função deve devolver uma string com as instruções para
+atravesar o labirinto. As instruções podem ser 'N','S','E','O'.
+
+'''
+
