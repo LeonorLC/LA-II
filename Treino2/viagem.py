4f831a9b563361c24e272c3fe6a0e14d8e88afc7
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-03-12 22:51:53 +0000

    Create viagem.py

---
+++ Treino2/viagem.py
@@ -0,0 +1,10 @@
+'''
+
+Implemente uma função que calcula o preço mais barato para fazer uma viagem de
+autocarro entre duas cidades. A função recebe (para além das duas cidades) uma
+lista de rotas de autocarro, onde cada rota é uma sequência de cidades por onde
+passa o autocarro, intercalada com o custo para viajar entre cada par de cidades.
+Assuma que cada rota funciona nos dois sentidos.
+
+'''
+
