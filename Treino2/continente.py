12ba0f60756fa8b889a7c21bb006056793dd17ee
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-03-13 20:48:45 +0000

    Update continente.py

--- Treino2/continente.py
+++ Treino2/continente.py
@@ -8,3 +8,46 @@ A função deverá devolver o tamanho do maior continente.
 
 '''
 
+def maior(vizinhos):
+    
+    if len(vizinhos) == 0:
+        return 0
+    
+    continentes = [vizinhos[0]]
+    paisesVisitados = []
+    
+    for v in vizinhos[0]:
+        paisesVisitados.append(v)
+        
+    vizinhos.pop(0)
+    
+    
+    
+    for viz in vizinhos:
+        acrescenta = 0
+        for pais in viz:
+            if pais in paisesVisitados:
+                acrescenta = 1
+                for continente in continentes:
+                    if pais in continente:
+                        for p in viz:
+                            if p not in continente:
+                                continente.append(p)
+                                paisesVisitados.append(p)
+                break
+        if acrescenta == 0:
+            continentes.append(viz)
+        else: 
+            acrescenta = 0
+                    
+    continentes
+    
+    comp = 0
+    for continente in continentes:
+        if comp < len(continente):
+            comp = len(continente)
+    
+    
+    return comp
+  
+ #Só passa 5% de 13% dos testes
