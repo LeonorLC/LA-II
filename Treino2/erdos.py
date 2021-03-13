30e5415fcf9fab71eaf29e6b928ecfb304a0da1c
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-03-13 16:11:57 +0000

    Update erdos.py

--- Treino2/erdos.py
+++ Treino2/erdos.py
@@ -12,3 +12,40 @@ autores com o mesmo número, lexicograficamente.
 
 '''
 
+def tratouAutores(autores, erdos, visitados):
+    for erdo in erdos:
+        if erdo[0] in autores:
+            for autor in autores:
+                if autor not in visitados:
+                    erdos.append((autor, erdo[1]+1))
+                    visitados.append(autor)
+                    erdos=sorted(erdos, key=lambda x: (x[1],x[0]))
+            return (erdos, True)
+    return (erdos, False)
+    
+def acabou(listaAutores, visitados):
+    for autores in listaAutores:
+        for autor in autores:
+            if autor in visitados:
+                return False
+    return True
+
+def erdos(artigos,n):
+    erdos=[('Paul Erdos', 0)]
+    visitados=['Paul Erdos']
+    listaAutores=list(artigos.values())
+    while listaAutores and not acabou(listaAutores, visitados):
+        erdos, tratou = tratouAutores(listaAutores[0],erdos, visitados)
+        if tratou:
+            listaAutores.pop(0)
+        else:
+            listaAutores.append(listaAutores.pop(0))
+    
+    erdosF=[]
+    for erdo in erdos:
+        if erdo[1] <= n:
+            erdosF.append(erdo[0])
+        else:
+            break
+    
+    return erdosF
