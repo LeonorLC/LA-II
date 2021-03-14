d51c3282698110dddc5fb02b05f9d635fde10e18
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-03-14 16:58:01 +0000

    Update cidade.py

--- Treino2/cidade.py
+++ Treino2/cidade.py
@@ -11,3 +11,59 @@ identificado pelo primeiro (e último) caracter do respectivo nome.
 
 '''
 
+#Função build dos slides (teórica 2)
+def build(arestas):
+    adj = {}
+    for o,d,p in arestas:
+        if o not in adj:
+            adj[o] = {}
+        if d not in adj:
+            adj[d] = {}
+        adj[o][d] = p
+        adj[d][o] = p
+    return adj
+
+#Função Floyd-Warshall dos slides (teórica 2)
+def fw(adj):
+    dist = {}
+    for o in adj:
+        dist[o] = {}
+        for d in adj:
+            if o == d:
+                dist[o][d] = 0
+            elif d in adj[o]:
+                dist[o][d] = adj[o][d]
+            else:
+                dist[o][d] = float("inf")
+    for k in adj:
+        for o in adj:
+            for d in adj:
+                if dist[o][k] + dist[k][d] < dist[o][d]:
+                    dist[o][d] = dist[o][k] + dist[k][d]
+    return dist
+    
+def tamanho(ruas):
+    arestas=[]
+    cruzamentos=[]
+    for rua in ruas:
+        if rua[0]+rua[-1] not in cruzamentos:
+            arestas.append((rua[0], rua[-1], len(rua)))
+            cruzamentos.append(rua[0]+rua[-1])
+        else:
+            for aresta in arestas:
+                if rua[0] == aresta[0] and rua[-1] == aresta[1] and len(rua) < aresta[2]:
+                    arestas.remove(aresta)
+                    arestas.append((rua[0], rua[-1], len(rua)))
+                    break
+
+    pesosDic = fw(build(arestas))
+    maior = 0
+    for o in pesosDic:
+        for d in pesosDic[o]:
+            dist = pesosDic[o][d]
+            if dist > maior:
+                maior = dist
+
+    return maior
+  
+  #Passa os testes todos (13%)
