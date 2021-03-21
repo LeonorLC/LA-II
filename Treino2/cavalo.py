f82fcca924c32643a7120c6645878ddec9209e09
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-03-21 15:02:31 +0000

    Update cavalo.py

--- Treino2/cavalo.py
+++ Treino2/cavalo.py
@@ -8,3 +8,29 @@ Assuma que o tabuleiro tem tamanho ilimitado.
 
 '''
 
+import collections
+def movimentos(x, y, m, movs):
+    dx = [1,-1,1,-1,2,2,-2,-2]
+    dy = [2,2,-2,-2,-1,1,-1,1]
+    
+    for i in range(8):
+        newX = x + dx[i]
+        newY = y + dy[i]
+        
+        movs.append( ((newX,newY), m+1) )
+    
+    return movs
+
+def saltos(o,d):
+    orla = collections.deque([(o,0)])
+    
+    while orla:
+        pos,mov = orla.popleft()
+        if pos == d:
+            return mov
+            
+        movimentos(pos[0], pos[1], mov, orla)
+    
+    return 0
+  
+#Passa 10% de 13% dos testes
