b654e49bba4d73c9ce1ba9f05daa0f95864e69d2
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-04-10 15:05:27 +0100

    Create validas.py

---
+++ Treino3/validas.py
@@ -0,0 +1,10 @@
+"""
+
+Um exemplo de um problema que pode ser resolvido de forma eficiente com 
+programação dinâmica consiste em determinar, dada uma sequência arbitrária de 
+números não negativos, se existe uma sub-sequência (não necessariamente contígua) 
+cuja soma é um determinado valor. Implemente uma função que dado um valor e uma
+listas de listas de números não negativos, devolva a lista com as listas com uma
+sub-sequência cuja soma é o valor dado.
+
+"""
