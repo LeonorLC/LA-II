21520cab9d1ec0469f14cd65e94f9f16b7a24680
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-04-10 15:02:38 +0100

    Create crescente.py

---
+++ Treino3/crescente.py
@@ -0,0 +1,12 @@
+"""
+
+Implemente uma função que dada uma sequência de inteiros, determinar o 
+comprimento da maior sub-sequência (não necessariamente contígua) que se 
+encontra ordenada de forma crescente.
+
+Sugere-se que comece por implementar uma função auxiliar recursiva que determina 
+o comprimento da maior sub-sequência crescente que inclui o primeiro elemento
+da sequência, sendo o resultado pretendido o máximo obtido aplicando esta
+função a todos os sufixos da sequência de entrada.
+
+"""
