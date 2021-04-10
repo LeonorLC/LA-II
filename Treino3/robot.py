866e0fef6a9e84702521b85acaf474bbbcc4ced7
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-04-10 15:04:14 +0100

    Create robot.py

---
+++ Treino3/robot.py
@@ -0,0 +1,11 @@
+"""
+
+Implemente uma função que determina qual a probabilidade de um robot regressar 
+ao ponto de partido num determinado número de passos. Sempre que o robot dá um 
+passo tem uma determinada probabilidade de seguir para cima ('U'), baixo ('D'), 
+esquerda ('L') ou direita ('R'). A função recebe o número de passos que o 
+robot vai dar e um dicionário com probabilidades de se movimentar em cada uma
+das direcções (as chaves são os caracteres indicados entre parêntesis).
+O resultado deve ser devolvido com a precisao de 2 casas decimais.
+
+"""
