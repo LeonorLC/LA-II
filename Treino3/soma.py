9f69ca5159df389744467214f34b9cfb4b18a257
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-04-10 15:05:04 +0100

    Create soma.py

---
+++ Treino3/soma.py
@@ -0,0 +1,12 @@
+"""
+
+Implemente uma função que calula qual a subsequência (contígua e não vazia) de 
+uma sequência de inteiros (também não vazia) com a maior soma. A função deve 
+devolver apenas o valor dessa maior soma.
+
+Sugere-se que começe por implementar (usando recursividade) uma função que 
+calcula o prefixo de uma sequência com a maior soma. Tendo essa função 
+implementada, é relativamente adaptá-la para devolver também a maior soma de toda
+a lista.
+
+"""
