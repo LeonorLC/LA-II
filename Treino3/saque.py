7d8014b861e0cd77fad43c2e5f90a1160c450cf5
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-04-10 15:04:35 +0100

    Create saque.py

---
+++ Treino3/saque.py
@@ -0,0 +1,17 @@
+"""
+
+Um fugitivo pretende atravessar um campo  no mínimo tempo possível (desde o 
+canto superior esquerdo até ao canto inferior direito). Para tal só se poderá 
+deslocar para a direita ou para baixo. No entanto, enquanto atravessa o campo 
+pretende saquear ao máximo os bens deixados por fugitivos anteriores. Neste 
+problema pretende-se que implemente uma função para determinar qual o máximo 
+valor que o fugitivo consegue saquear enquanto atravessa o campo. 
+A função recebe o mapa rectangular defindo com uma lista de strings. Nestas
+strings o caracter '.' representa um espaço vazio, o caracter '#' representa 
+um muro que não pode ser atravessado, e os digitos sinalizam posições onde há 
+bens abandonados, sendo o valor dos mesmos igual ao digito.
+Deverá devolver o valor máximo que o fugitivo consegue saquear enquanto 
+atravessa o campo deslocando-se apenas para a direita e para baixo. Assuma que 
+é sempre possível atravessar o campo dessa forma.
+
+"""
