89e78890016866661144c2a0288f9fb0469e7c7c
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-04-10 15:03:24 +0100

    Create espaca.py

---
+++ Treino3/espaca.py
@@ -0,0 +1,11 @@
+"""
+
+Implemente uma função que, dada uma frase cujos espaços foram retirados, 
+tenta recuperar a dita frase. Para além da frase (sem espaços nem pontuação), 
+a função recebe uma lista de palavras válidas a considerar na reconstrução 
+da dita frase. Deverá devolver a maior frase que pode construir inserindo
+espaços na string de entrada e usando apenas as palavras que foram indicadas 
+como válidas. Por maior entende-se a que recupera o maior prefixo da string
+de entrada. Só serão usados testes em que a maior frase é única.
+
+"""
