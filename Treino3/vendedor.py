240d3e7fb731ec023ae1c8c4abe0e94c6a4b582d
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-04-13 17:55:50 +0100

    4%

--- Treino3/vendedor.py
+++ Treino3/vendedor.py
@@ -19,3 +19,32 @@ Caso haja 2 produtos com a mesma rentabilidade por peso deverá dar prioridade
 aos produtos que aparecem primeiro na lista de entrada.
 
 """
+def aux(capacidade, produtos, p):
+    if produtos == [] or capacidade == 0:
+        return 0
+    if produtos[0][2] > capacidade:
+        return 0
+    ant = p[2]
+    lucro = p[1]
+    vendeu = [p[0]]
+    res = [(p[1], [p[0]])]
+    for prod in produtos:
+        if (ant + prod[2]) <= capacidade:
+            ant += prod[2]
+            lucro += prod[1]
+            vendeu.append(prod[0])
+            res.append((lucro, vendeu))
+    return max(res, key = lambda x: x[0]) 
+
+def vendedor(capacidade, produtos):
+    if produtos == [] or capacidade == 0:
+        return (0, [])
+    produtos = sorted(produtos, key = lambda x : x[0])
+    d = {}
+    vendeu = []
+    for p in produtos:
+        if p[2] > capacidade:
+            produtos.remove(p)
+        else:
+            d[p] = (aux(capacidade, produtos, p))
+    return d[max(d.keys(), key=(lambda x: d[x]))]
