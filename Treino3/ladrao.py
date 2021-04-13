a3275059997ace528aa3089a76e75bd782c5b6e2
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-04-13 15:20:40 +0100

    9%

--- Treino3/ladrao.py
+++ Treino3/ladrao.py
@@ -10,3 +10,24 @@ venda no mercado negro, e o seu peso. Deve devolver o máximo lucro que o ladrã
 poderá  obter para a capacidade de carga especificada.
 
 """
+def aux(capacidade, objetos):
+    if objetos == [] or capacidade == 0:
+        return 0
+    objetos = sorted(objetos, key = lambda x : x[1], reverse = True)
+    ant = objetos[0][2]
+    lucro = objetos[0][1]
+    lucros = [objetos[0][1]]
+    for o in objetos[1:]:
+        if (ant + o[2]) <= capacidade:
+            ant += o[2]
+            lucro += o[1]
+            lucros.append(lucro)
+    return max(lucros)
+
+def ladrao(capacidade,objetos):
+    if objetos == [] or capacidade == 0:
+        return 0
+    d = {}
+    for i, o in enumerate(objetos):
+        d[o] = (aux(capacidade, objetos[i:]))
+    return d[max(d.keys(), key=(lambda x: d[x]))]
