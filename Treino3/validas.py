176d326849c3c43d8b8e306683c561a984ad7000
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-04-14 00:17:01 +0100

    10%

--- Treino3/validas.py
+++ Treino3/validas.py
@@ -8,3 +8,23 @@ listas de listas de números não negativos, devolva a lista com as listas com u
 sub-sequência cuja soma é o valor dado.
 
 """
+def somas(soma, lista):
+    if lista == [] or soma == 0:
+        return False
+    ant = lista[0]
+    for elem in lista:
+        if (ant + elem) == soma:
+            return True
+        elif (ant + elem) < soma:
+            ant += elem
+        else:
+            return False
+def validas(soma,listas):
+    if listas == [] or soma == 0:
+        return []
+    val = []
+    for lista in listas:
+        for i,elem in enumerate(lista):
+            if somas(soma, lista[i:]) == True and lista not in val:
+                val.append(lista)
+    return val
