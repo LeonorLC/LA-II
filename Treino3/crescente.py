48553774a5f8cc9e3ee7784e9a5a288c4c80ef95
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-04-16 23:22:15 +0100

    13%

--- Treino3/crescente.py
+++ Treino3/crescente.py
@@ -10,7 +10,22 @@ da sequência, sendo o resultado pretendido o máximo obtido aplicando esta
 função a todos os sufixos da sequência de entrada.
 
 """
+#Versão programação dinâmica (13%)
+def crescente(lista):
+    if len(lista) == 0:
+        return 0
+    comp = len(lista)
+    compCres = [1 for x in range(comp+1)]
+    compCres[0] = 0
+    maior = 1
+    for x in range(2, comp+1):
+        for y in range(x-1, 0, -1): #começa no x-1, vai até ao 0 decrementando 1
+            if lista[x-1] >= lista[y-1]:
+                compCres[x] = max(compCres[x], compCres[y] + 1)
+        maior = max(maior, compCres[x])
+    return maior
 
+#Versão Recursiva (9%)
 def maior(lista):
     if lista == []:
         return 0
