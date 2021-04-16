288137819e8db7ceb9dd09fc6d789604e8072648
LeonorLC <79547593+LeonorLC@users.noreply.github.com>
2021-04-16 19:05:17 +0100

    10%

--- Treino3/robot.py
+++ Treino3/robot.py
@@ -9,3 +9,31 @@ das direcções (as chaves são os caracteres indicados entre parêntesis).
 O resultado deve ser devolvido com a precisao de 2 casas decimais.
 
 """
+def probabilidade(passos, probs):
+    if passos == 0 or probs == {}:
+        return 0
+    pro = {}
+    limiteU = passos
+    limiteD = -passos
+    posX = [0, 0, 1,-1]
+    posY = [1, -1, 0, 0]
+    movs = ['U', 'D', 'R', 'L']
+    res = 0
+    
+    for x in range(limiteD, limiteU+1):
+        for y in range(limiteD, limiteU+1): 
+            for p in range(0, passos+1):
+                pro[p, x, y] = 0.0
+                
+    pro[0,0,0] = 1.0
+    
+    for passo in range(1, passos+1):
+        for x in range(limiteD, limiteU):
+            for y in range(limiteD, limiteU):
+                for i in range(4):
+                    if limiteD <= (x + posX[i]) <= limiteU and limiteD <= (y + posY[i]) <= limiteU:
+                        anterior = probs[movs[i]]*pro[passo-1, x + posX[i], y + posY[i]]
+                        pro[passo, x, y] += anterior
+                    
+    
+    return round(pro[passos,0,0],2)
